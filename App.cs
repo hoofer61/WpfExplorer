4cb163b876655cdb3a9bf924514077dadd05bb26
hoofer61 <hoofer61@nate.com>
2023-08-29 23:28:11 +0900

    초기 커밋.

---
+++ App.cs
@@ -0,0 +1,14 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+
+namespace WpfExplorer
+{
+    internal class App : JamesApplication
+    {
+        protected override Window CreateShell()
+        {
+            //Temporary Window instance
+            return new Window();
+        }
+    }
+}
